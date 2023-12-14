bca69dcea02b7860e7d1b1aeaa92506e853115f8
Alejandro Gullón <agullon@redhat.com>
2023-12-14 15:27:30 +0100

    Update configure.sh

--- scripts/image-builder/configure.sh
+++ scripts/image-builder/configure.sh
@@ -21,8 +21,8 @@ sudo dnf copr enable -y @osbuild/osbuild epel-9-"$(uname -m)"
 sudo dnf copr enable -y @osbuild/osbuild-composer rhel-9-"$(uname -m)"
 
 sudo dnf install -y \
-    osbuild-composer-95 \
-    osbuild-100 \
+    osbuild-composer-96-1.20231213092324044071.main.10.g991293a89.el9 \
+    osbuild-101-1.20231213161521815551.main.24.g05c0fd31.el9 \
     git composer-cli ostree rpm-ostree \
     cockpit-composer bash-completion podman runc genisoimage \
     createrepo yum-utils selinux-policy-devel jq wget lorax rpm-build \
